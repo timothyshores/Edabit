2a5399528f189044fb11da1db239f7dfa1366347
Timothy Shores <timothymshores@gmail.com>
2020-03-27 15:12:40 -0500

    Add Shapes With N Sides solution.js

---
+++ Shapes With N Sides/solution.js
@@ -0,0 +1,15 @@
+const nSidedShape = n => {
+	const table = {
+		1: "circle",
+		2: "semi-circle",
+		3: "triangle",
+		4: "square",
+		5: "pentagon",
+		6: "hexagon",
+		7: "heptagon",
+		8: "octagon",
+		9: "nonagon",
+		10: "decagon"
+	};
+	return table[n];
+};
